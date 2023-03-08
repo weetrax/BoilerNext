cad4e5fe73872f1b5a27f36bb5588da2209568e1
smartines <stefano.martines@sequoiasoft.com>
2023-03-08 14:55:40 +0100

    hooks refactor & html semantic fix

--- components/_Layout/index.tsx
+++ components/_Layout/index.tsx
@@ -1,9 +1,9 @@
-import Footer from './Footer';
-import Navbar from './Navbar';
-import NextNprogress from 'nextjs-progressbar';
-import React from 'react';
-import { Toaster } from 'react-hot-toast';
-import { useTheme } from '../../hooks/useTheme';
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import NextNprogress from "nextjs-progressbar";
+import React from "react";
+import { Toaster } from "react-hot-toast";
+import { useTheme } from "../../hooks/useTheme";
 
 type _LayoutProps = {
   children: React.ReactNode;
@@ -14,23 +14,27 @@ const _Layout: React.FC<_LayoutProps> = ({ children }) => {
 
   return (
     <div className={theme}>
+      <Toaster position="top-center" reverseOrder={false} />
+      <NextNprogress
+        color="#fca311"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        options={{ showSpinner: false }}
+        showOnShallow={true}
+      />
       <div
         className={
           "flex flex-col min-h-screen bg-gray-50 dark:bg-dark-500 text-black dark:text-white font-body"
         }
       >
-        <Navbar />
-        <Toaster position="top-center" reverseOrder={false} />
-        <NextNprogress
-          color="#fca311"
-          startPosition={0.3}
-          stopDelayMs={200}
-          height={3}
-          options={{ showSpinner: false }}
-          showOnShallow={true}
-        />
+        <header>
+          <Navbar />
+        </header>
         <main className="flex-1">{children}</main>
-        <Footer />
+        <footer>
+          <Footer />
+        </footer>
       </div>
     </div>
   );
